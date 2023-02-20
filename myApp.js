b59b437ab969329c24b678dfe29550ad41e46d12
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2023-02-20 17:20:12 +1100

    set up mongo

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URI).then(console.log("mongo connected"));
 
 let Person;
 
